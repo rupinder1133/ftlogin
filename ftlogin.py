9b6c145b4995ec129fc2f4de823e42b03f10d1e2
Rupinder Singh <rupinder1133@gmail.com>
2016-07-07 17:33:01 +0530

    added comments in code

--- ftlogin.py
+++ ftlogin.py
@@ -2,22 +2,22 @@ from requests import post
 from urllib import urlopen
 from re import search
 from time import sleep
-d={	"magic":"key",
-	"username":"YOUR_USER_NAME",
-	"password":"YOUR_PASSWORD"}
+d={	"magic":"key",				# magic key , unique to every login attempt
+	"username":"YOUR_USER_NAME",		# USERNAME
+	"password":"YOUR_PASSWORD"}		# PASSWORD
 
 def site():
-    try:
+    try:					# try to open any website to open the firewall login page
         urlopen("http://aitpune.com").read()
-    except:
+    except:					# if fails , try again aafter 1.5 secs ( might fail due to internet issues
         sleep(1.5)
         site()
 
 site()
 try:
-    a=urlopen("http://aitpune.com").read()
-    d['magic']=search(r'name="magic" value="(.+)">',a).group(1)
-    post("http://aitpune.com/",data=d)
+    a=urlopen("http://aitpune.com").read()		# copy HTML to 'a'
+    d['magic']=search(r'name="magic" value="(.+)">',a).group(1)		# extract 'magic' key and store in dictionary
+    post("http://aitpune.com/",data=d)				# send login form as POST with data in dictionary 'd'
     print "LOGGED IN SUCCESSFULLY"
 except:
     print "FAILED"
